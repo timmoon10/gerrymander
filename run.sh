f63478aeab30da2e50eba0c7fd60f6e118e737bb
Tim Moon <timoteo.moon@gmail.com>
2022-08-25 22:42:51 -0700

    Rename script for discrete partition relaxation

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 pushd $(dirname $(realpath $0))
-julia parse_population_data.jl
-julia parse_geography_data.jl
-julia construct_interaction_graph.jl
-julia construct_geography_graph.jl
-julia relax_partitions.jl
+#julia parse_population_data.jl
+#julia parse_geography_data.jl
+#julia construct_interaction_graph.jl
+#julia construct_geography_graph.jl
+julia relax_partitions_softmax.jl $@
 julia plot_results.jl
 popd
