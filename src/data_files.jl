f86398067deec7180ff8747d68db09410290cac2
Tim Moon <tmoon@nvidia.com>
2024-10-03 19:14:57 -0700

    Get ogr2ogr from GDAL package

--- src/data_files.jl
+++ src/data_files.jl
@@ -3,6 +3,7 @@ module DataFiles
 import DataStructures
 import DelimitedFiles
 import Downloads
+import GDAL_jll
 import JSON
 import Memoize
 
@@ -27,34 +28,51 @@ end
 end
 
 function maybe_download_census_block_data(state_id::UInt)::String
+
+    # Return immediately if census block data is ready
     file_base = "tl_2020_" * lpad(state_id, 2, "0") * "_tabblock20"
     download_dir = joinpath(root_dir(), "data", "downloads")
     json_file = joinpath(download_dir, file_base * ".json")
-    if !isfile(json_file)
-        println("Downloading census block data for " * state_names()[state_id] * "...")
-        census_url = "https://www2.census.gov/geo/tiger/TIGER2020/TABBLOCK20/"
-        zip_file = joinpath(download_dir, file_base * ".zip")
-        shape_file = joinpath(download_dir, file_base * ".shp")
-        Downloads.download(census_url * file_base * ".zip", zip_file)
-        run(`unzip $zip_file -d $download_dir`)
-        run(`ogr2ogr -f "GeoJSON" $json_file $shape_file`)
+    if isfile(json_file)
+        return json_file
     end
+
+    # Download data
+    println("Downloading census block data for " * state_names()[state_id] * "...")
+    census_url = "https://www2.census.gov/geo/tiger/TIGER2020/TABBLOCK20/"
+    zip_file = joinpath(download_dir, file_base * ".zip")
+    Downloads.download(census_url * file_base * ".zip", zip_file)
+
+    # Extract and parse shape file
+    shape_file = joinpath(download_dir, file_base * ".shp")
+    run(`unzip $zip_file -d $download_dir`)
+    run(`$(GDAL_jll.ogr2ogr_path()) -f "GeoJSON" $json_file $shape_file`)
+
     return json_file
+
 end
 
 function maybe_download_geography_data()::String
+
+    # Return immediately if census block data is ready
     file_base = "tl_2020_us_county"
     download_dir = joinpath(root_dir(), "data", "downloads")
     json_file = joinpath(download_dir, file_base * ".json")
-    if !isfile(json_file)
-        println("Downloading geography data...")
-        census_url = "https://www2.census.gov/geo/tiger/TIGER2020/COUNTY/"
-        zip_file = joinpath(download_dir, file_base * ".zip")
-        shape_file = joinpath(download_dir, file_base * ".shp")
-        Downloads.download(census_url * file_base * ".zip", zip_file)
-        run(`unzip $zip_file -d $download_dir`)
-        run(`ogr2ogr -f "GeoJSON" $json_file $shape_file`)
+    if isfile(json_file)
+        return json_file
     end
+
+    # Download data
+    println("Downloading geography data...")
+    census_url = "https://www2.census.gov/geo/tiger/TIGER2020/COUNTY/"
+    zip_file = joinpath(download_dir, file_base * ".zip")
+    Downloads.download(census_url * file_base * ".zip", zip_file)
+
+    # Extract and parse shape file
+    shape_file = joinpath(download_dir, file_base * ".shp")
+    run(`unzip $zip_file -d $download_dir`)
+    run(`$(GDAL_jll.ogr2ogr_path()) -f "GeoJSON" $json_file $shape_file`)
+
     return json_file
 end
 
@@ -75,13 +93,27 @@ function maybe_parse_county_populations(state_ids::AbstractVector{UInt})::String
         maybe_download_census_block_data(state_id)
         for state_id in state_ids]
 
+    function coords_sum(
+        coords::Vector{Any},
+        )::Tuple{Float64, Float64, Float64}
+        deg_to_rad::Float64 = pi / 180
+        sum_x::Float64 = 0
+        sum_y::Float64 = 0
+        sum_z::Float64 = 0
+        for coord::Vector{Float64} in coords
+            long::Float64 = deg_to_rad * coord[1]
+            lat::Float64 = deg_to_rad * coord[2]
+            sum_x += cos(long) * sin(lat)
+            sum_y += sin(long) * sin(lat)
+            sum_z += cos(lat)
+        end
+        return (sum_x, sum_y, sum_z)
+    end
+
     function parse_state_data(
         json_file::String,
         state_id::UInt,
         )::Array{Any, 2}
-
-        # Constants
-        deg_to_rad::Float64 = pi / 180
 
         # Parse JSON file to get position and population of census blocks
         sums = Dict{UInt, Array{Float64}}()
@@ -95,26 +127,20 @@ function maybe_parse_county_populations(state_ids::AbstractVector{UInt})::String
             geometry = block_json["geometry"]["type"]
             if geometry == "Polygon"
                 for polygon::Vector{Any} in block_json["geometry"]["coordinates"]
-                    for coord::Vector{Float64} in polygon
-                        long::Float64 = deg_to_rad * coord[1]
-                        lat::Float64 = deg_to_rad * coord[2]
-                        sum_x += cos(long) * sin(lat)
-                        sum_y += sin(long) * sin(lat)
-                        sum_z += cos(lat)
-                        num_coords += 1
-                    end
+                    (sx, sy, sz) = coords_sum(polygon)
+                    sum_x += sx
+                    sum_y += sy
+                    sum_z += sz
+                    num_coords += size(polygon, 1)
                 end
             elseif geometry == "MultiPolygon"
                 for multipolygon::Vector{Any} in block_json["geometry"]["coordinates"]
                     for polygon::Vector{Any} in multipolygon
-                        for coord::Vector{Float64} in polygon
-                            long::Float64 = deg_to_rad * coord[1]
-                            lat::Float64 = deg_to_rad * coord[2]
-                            sum_x += cos(long) * sin(lat)
-                            sum_y += sin(long) * sin(lat)
-                            sum_z += cos(lat)
-                            num_coords += 1
-                        end
+                        (sx, sy, sz) = coords_sum(polygon)
+                        sum_x += sx
+                        sum_y += sy
+                        sum_z += sz
+                        num_coords += size(polygon, 1)
                     end
                 end
             end
@@ -124,13 +150,13 @@ function maybe_parse_county_populations(state_ids::AbstractVector{UInt})::String
 
             # Initialize county data
             props_json = block_json["properties"]
+            pop::Float64 = props_json["POP20"]
             county_id = parse(UInt, props_json["COUNTYFP20"])
             geoid::UInt = state_id * 1000 + county_id
             if !haskey(sums, geoid)
                 sums[geoid] = zeros(Float64, 7)
             end
             county_sums = sums[geoid]
-            pop::Float64 = props_json["POP20"]
 
             # Record population and position of census block
             county_sums[1] += pop * x
@@ -143,7 +169,7 @@ function maybe_parse_county_populations(state_ids::AbstractVector{UInt})::String
 
         end
 
-        # Compute population, votes, and position of counties
+        # Compute population and position of counties
         state_county_data = Array{Any, 2}(undef, length(sums), 6)
         for (i, (geoid, county_sums)) in enumerate(sums)
             pop = county_sums[7]
@@ -155,7 +181,7 @@ function maybe_parse_county_populations(state_ids::AbstractVector{UInt})::String
             var_z = county_sums[6] / pop - z * z
             var = max(var_x + var_y + var_z, 0.0)
             state_county_data[i, 1] = geoid
-            state_county_data[i, 2] = pop
+            state_county_data[i, 2] = UInt(pop)
             state_county_data[i, 3] = x
             state_county_data[i, 4] = y
             state_county_data[i, 5] = z
@@ -178,6 +204,5 @@ function maybe_parse_county_populations(state_ids::AbstractVector{UInt})::String
     return county_populations_file
 
 end
-
 
 end  # module DataFiles
--- src/main.jl
+++ src/main.jl
@@ -1,7 +1,11 @@
 include(joinpath(@__DIR__, "Gerrymander.jl"))
 
-state_id::UInt = 34
-state_name = Gerrymander.DataFiles.state_names()[state_id]
-println("State ", state_id, " is ", state_name)
-Gerrymander.DataFiles.maybe_parse_county_populations([state_id])
-Gerrymander.DataFiles.maybe_download_geography_data()
+function main()
+    state_id::UInt = 34
+    state_name = Gerrymander.DataFiles.state_names()[state_id]
+    println("State ", state_id, " is ", state_name)
+    Gerrymander.DataFiles.maybe_parse_county_populations([state_id])
+    Gerrymander.DataFiles.maybe_download_geography_data()
+end
+
+main()
