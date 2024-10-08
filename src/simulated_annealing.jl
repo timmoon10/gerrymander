0511738f67ad5bad3ef7a2c9c4ee1dcbd6bf72cb
Tim Moon <timoteo.moon@gmail.com>
2024-10-08 00:13:20 -0700

    Add function to make partition contiguous

--- src/simulated_annealing.jl
+++ src/simulated_annealing.jl
@@ -1,11 +1,64 @@
 module SimulatedAnnealing
 
+import DataStructures
 import Random
 
 import ..DataFiles
 import ..DataGraphs
 import ..Graph
 import ..Plot
+
+function contiguous_partition!(
+    county_to_partition::Dict{UInt, UInt},
+    partition_to_counties::Dict{UInt, Set{UInt}},
+    partition_center_county::Dict{UInt, UInt},
+    graph::Graph.WeightedGraph,
+    )
+
+    # Iterate through partitions
+    for partition_id in keys(partition_to_counties)
+
+        # BFS from partition center
+        center_id = partition_center_county[partition_id]
+        found_set = Set{UInt}([center_id])
+        search_queue = DataStructures.Queue{UInt}()
+        DataStructures.enqueue!(search_queue, center_id)
+        while !isempty(search_queue)
+            current = DataStructures.dequeue!(search_queue)
+            for neighbor in keys(graph[current])
+                if !in(neighbor, found_set)
+                    push!(found_set, neighbor)
+                    DataStructures.enqueue!(search_queue, neighbor)
+                end
+            end
+        end
+
+        # Identify disconnected counties
+        partition_counties = partition_to_counties[partition_id]
+        disconnected = setdiff(partition_counties, found_set)
+        setdiff!(partition_counties, disconnected)
+
+        # Reassign disconnected counties to neighboring partitions
+        while !isempty(disconnected)
+            for county_id in collect(disconnected)
+                for neighbor in keys(graph[county_id])
+                    neighbor_partition_id = county_to_partition[neighbor]
+                    if neighbor == partition_id
+                        continue
+                    end
+                    county_to_partition[county_id] = neighbor_partition_id
+                    push!(
+                        partition_to_counties[neighbor_partition_id],
+                        county_id,
+                    )
+                    break
+                end
+            end
+        end
+
+    end
+
+end
 
 function voronoi_partition(
     num_partitions::UInt,
@@ -58,7 +111,17 @@ function voronoi_partition(
         push!(partition_to_counties[partition_id], county_id)
     end
 
-    ### TODO Enforce contiguousness
+    # Make sure partitions are contiguous
+    graph = DataGraphs.county_adjacency_graph(state_ids)
+    center_ids = Dict{UInt, UInt}(
+        partition_id => county_id
+        for (partition_id, county_id) in enumerate(center_ids))
+    contiguous_partition!(
+        county_to_partition,
+        partition_to_counties,
+        center_ids,
+        DataGraphs.county_adjacency_graph(state_ids),
+    )
 
     return (county_to_partition, partition_to_counties)
 
